ff99dc1003c2cebb8ec8f0a93596bd42e9ca024f
ScoutGhost <scoutghostgamr@gmail.com>
2018-12-06 09:20:00 -0600

    eliminaciones
    se eliminaron los archivos css y js que traia la informacion de Favela
    
    Co-Authored-By: jeffersongutierritos <jeffersongutierritos@users.noreply.github.com>

--- resources/views/inicio/Inicio.blade.php
+++ resources/views/inicio/Inicio.blade.php
@@ -19,132 +19,62 @@
     @parent
     @show
     @section('slider')
-  <div class="carousel carousel-slider">
-    <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"></a>
-    <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"></a>
-    <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"></a>
-    <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"></a>
-  </div>
+
+        <div class="carousel carousel-slider">
+            <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/800/400/food/1"></a>
+            <a class="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2"></a>
+            <a class="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3"></a>
+            <a class="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4"></a>
+        </div>
+
+<br>
 <div class="divider"></div>
-<div class="section center">
+<div class="center">
   <h2>Destacados</h2>
 </div>
 <div class="divider"></div>
+<br>
     @endsection
 
 
-    @section('content')
+    @section('catalogo')
     <div class="row">
       <div class="col s12">
         <div class="col s3 ">
-                        <div class="card z-depth-3">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title grey-text text-darken-4">Playera Chida</span>
-                                <p style="color: #dd0007;">Precio: MXN$200</p>
-                            </div>
-                            <div class="card-action">
-                                <form action="detalles" method="post">
-                                    {{csrf_field()}}
-                                    <input id="id" name="id" class="hide" type="text" value="">
-                                    <button  type="submit" class="btn center-block waves-effect waves-block grey darken-4 z-depth-3">
-                                        Ver detalles
-                                    </button>
+                <div class="card hoverable">
+                  <div class="card-image">
+                    <a href="#"><img src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg"></a>
+                    <span class="card-title">Card Title</span>
+                  </div>
+                </div>
+        </div>
 
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+        <div class="col s3 ">
+            <div class="card hoverable">
+                <div class="card-image">
+                 <a href="#"><img src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg"></a>
+                  <span class="card-title">Card Title</span>
+                </div>
+            </div>
+        </div>
 
-                    <div class="col s3 ">
-                        <div class="card z-depth-3">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title grey-text text-darken-4">Playera Chida</span>
-                                <p style="color: #dd0007;">Precio: MXN$200</p>
-                            </div>
-                            <div class="card-action">
-                                <form action="detalles" method="post">
-                                    {{csrf_field()}}
-                                    <input id="id" name="id" class="hide" type="text" value="">
-                                    <button  type="submit" class="btn center-block waves-effect waves-block grey darken-4 z-depth-3">
-                                        Ver detalles
-                                    </button>
+        <div class="col s3 ">
+            <div class="card hoverable">
+                <div class="card-image">
+                 <a href="#"><img src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg"></a>
+                  <span class="card-title">Card Title</span>
+                </div>
+            </div>
+        </div>
 
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col s3 ">
-                        <div class="card z-depth-3">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title grey-text text-darken-4">Playera Chida</span>
-                                <p style="color: #dd0007;">Precio: MXN$200</p>
-                            </div>
-                            <div class="card-action">
-                                <form action="detalles" method="post">
-                                    {{csrf_field()}}
-                                    <input id="id" name="id" class="hide" type="text" value="">
-                                    <button  type="submit" class="btn center-block waves-effect waves-block grey darken-4 z-depth-3">
-                                        Ver detalles
-                                    </button>
-
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col s3 ">
-                        <div class="card z-depth-3">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title grey-text text-darken-4">Playera Chida</span>
-                                <p style="color: #dd0007;">Precio: MXN$200</p>
-                            </div>
-                            <div class="card-action">
-                                <form action="detalles" method="post">
-                                    {{csrf_field()}}
-                                    <input id="id" name="id" class="hide" type="text" value="">
-                                    <button  type="submit" class="btn center-block waves-effect waves-block grey darken-4 z-depth-3">
-                                        Ver detalles
-                                    </button>
-
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col s3 ">
-                        <div class="card z-depth-3">
-                            <div class="card-image waves-effect waves-block waves-light">
-                                <img class="activator" src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg">
-                            </div>
-                            <div class="card-content">
-                                <span class="card-title grey-text text-darken-4">Playera Chida</span>
-                                <p style="color: #dd0007;">Precio: MXN$200</p>
-                            </div>
-                            <div class="card-action">
-                                <form action="detalles" method="post">
-                                    {{csrf_field()}}
-                                    <input id="id" name="id" class="hide" type="text" value="">
-                                    <button  type="submit" class="btn center-block waves-effect waves-block grey darken-4 z-depth-3">
-                                        Ver detalles
-                                    </button>
-
-                                </form>
-                            </div>
-                        </div>
-                    </div>
+        <div class="col s3 ">
+            <div class="card hoverable">
+                <div class="card-image">
+                 <a href="#"><img src="images/b2e710b796a3d96a1482b705198941642c88f124.jpg"></a>
+                  <span class="card-title">Card Title</span>
+                </div>
+            </div>
+        </div>
       </div>
     </div>
     @endsection
