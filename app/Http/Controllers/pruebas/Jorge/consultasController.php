787125fdfe1f4a58dae20b94cda9eae2e2943222
unknown <jorge.argumaniz@gmail.com>
2018-12-07 12:27:44 -0600

    Carrito

--- app/Http/Controllers/pruebas/Jorge/consultasController.php
+++ app/Http/Controllers/pruebas/Jorge/consultasController.php
@@ -26,16 +26,25 @@ class consultasController extends Controller
         $id = $r->input('id');
         $talla = $r->get('tallas');
         $cantidad = $r->input('cantidad');
+//        $producto = DB::table('productos')->join('disenos','disenos.id_diseno','=','productos.id_diseno','inner')
+//            ->join('categorias','categorias.categoria','=','disenos.categoria','inner')->join('tipos_producto', 'tipos_producto.id_tipo_producto', '=','productos.id_tipo_producto')
+//            ->select(DB::raw("productos.sexo as 'sexo', productos.nombre as 'nombre', disenos.diseno as 'diseno', productos.costo_unitario as 'costo', tipos_producto.nombre as 'tipo', disenos.categoria as 'categoria', productos.id_producto as 'id_producto'"))
+//            ->where('productos.id_producto','=',$id)
+//            ->get();
         $producto = DB::table('productos')->join('disenos','disenos.id_diseno','=','productos.id_diseno','inner')
-            ->join('categorias','categorias.categoria','=','disenos.categoria','inner')->join('tipos_producto', 'tipos_producto.id_tipo_producto', '=','productos.id_tipo_producto')
-            ->select(DB::raw("productos.sexo as 'sexo', productos.nombre as 'nombre', disenos.diseno as 'diseno', productos.costo_unitario as 'costo', tipos_producto.nombre as 'tipo', disenos.categoria as 'categoria', productos.id_producto as 'id_producto'"))
-            ->where('productos.id_producto','=',$id)
-            ->get();
+           ->join('categorias','categorias.categoria','=','disenos.categoria','inner')->join('tipos_producto', 'tipos_producto.id_tipo_producto', '=','productos.id_tipo_producto')
+            ->join('carritos_has_productos', 'carritos_has_productos.id_producto','=','productos.id_producto')
+            ->join('carritos','carritos.id_carrito','=','carritos_has_productos.id_carrito')
+           ->select(DB::raw("id_carrito.carritos as 'id_carrito', productos.sexo as 'sexo', productos.nombre as 'nombre', disenos.diseno as 'diseno', productos.costo_unitario as 'costo', tipos_producto.nombre as 'tipo', disenos.categoria as 'categoria', productos.id_producto as 'id_producto'"))
+           ->where('productos.id_producto','=',4)
+           ->get();
+
 
         $carrito = new Carrito();
-        $carrito->id_carrito =  '4';
-        $carrito->save();
-        $carrito->producto_carrito();
+        $carrito->producto_carrito()->id_carrito = '4';
+        $carrito->producto_carrito()->id_producto = $id;
+        $carrito->producto_carrito()->cantidad = dd($carrito->producto_carrito()->sum("carrito_has_productos.cantidad as 'cantidad'")->where('id_carrito','=',$id)->get());
+
         return view('jorgeViews.carrito')->with('producto', $producto)->with('cantidad', $cantidad)->with('talla', $talla);
     }
 
--- resources/views/jorgeViews/carrito.blade.php
+++ resources/views/jorgeViews/carrito.blade.php
@@ -1,8 +1,8 @@
 @extends('base')
 @section('content')
     <h4 align="center"><strong>Productos en el carrito</strong></h4>
+    @foreach($carrito as $producto)
     <div class="row card z-depth-3" style="padding-bottom: 30px; margin-top: 10px">
-
         <br>
         <div class="col m4 s4" align="center">
             <p style="font-size: 20px"><strong>{{$producto[0]->nombre}}</strong></p>
@@ -20,6 +20,7 @@
             <strong>MXN${{$cantidad*$producto[0]->costo}}</strong>
         </div>
     </div>
+    @endforeach
     <div class="row">
         <div class="col m6 m6 offset-m8 offset-s8">
             <h4><strong>Subtotal: MXN$126</strong></h4>
