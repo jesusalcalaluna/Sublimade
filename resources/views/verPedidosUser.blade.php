3f2d2577eccc8ca507a11c7181b6dfeabe3e6282
unknown <jorge.argumaniz@gmail.com>
2018-12-17 02:25:33 -0600

    Correcion

--- resources/views/verPedidosUser.blade.php
+++ resources/views/verPedidosUser.blade.php
@@ -1,7 +1,7 @@
 @extends('base')
 @section('content')
     <div class="row">
-        <div class="col m10 offset-m1">
+        <div class="col m12">
             <div class="center">
                 <h4><strong>Mis pedidos</strong></h4><br>
                 <div class="divider">
@@ -11,19 +11,19 @@
                 <thead>
                 <tr>
                     <th>Registro</th>
-                    <th>Cliente</th>
-                    <th>F.pedido</th>
-                    <th>F.entrega</th>
+
+                    <th>Fecha de pedido</th>
+                    <th>Fecha de entrega</th>
                     <th>Detalles</th>
                     <th>Estado</th>
-                    <th>F.R.Entrega</th>
+                    <th>Fecha real de entrega</th>
                 </tr>
                 </thead>
                 <tbody  id="pedidos" style="color: black;" >
                 @foreach ($pedidos as $pedido)
                     <tr>
                         <td>{{$pedido->reg_pedido}}</td>
-                        <td>{{$pedido->nombre}} {{$pedido->apellido}}</td>
+
                         <td>{{$pedido->fecha_pedido}}</td>
                         <td>{{$pedido->fecha_entrega}}</td>
                         <td>{{$pedido->detalles}}</td>
