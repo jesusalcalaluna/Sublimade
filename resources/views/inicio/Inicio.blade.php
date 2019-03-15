1527fb313f8383a788655e9eac2b24093c0b4bd5
JeffersonGutierritos <arturo99favela@gmail.com>
2019-03-14 21:13:16 -0600

    arreglé el problema que había, creo, pero no se tiene acceso a la carpeta de storage en el proyecto en línea

--- resources/views/inicio/Inicio.blade.php
+++ resources/views/inicio/Inicio.blade.php
@@ -20,11 +20,11 @@
     @show
     @section('slider')
 
-        <div class="carousel carousel-slider">
-            <a class="carousel-item"><img src="{{url('/storage/app/public/Inicio/slider1.png')}}"></a>
-            <a class="carousel-item"><img src="{{url('/storage/app/public/Inicio/slider2.png')}}"></a>
-            <a class="carousel-item"><img src="{{url('/storage/app/public/Inicio/slider3.png')}}"></a>
-            <a class="carousel-item"><img src="{{url('/storage/app/public/Inicio/slider4.png')}}"></a>
+            <div class="carousel carousel-slider">
+                <a class="carousel-item"><img src="{{url('storage/app/public/Inicio/slider1.png')}}"></a>
+            <a class="carousel-item"><img src="{{url('storage/app/public/Inicio/slider2.png')}}"></a>
+            <a class="carousel-item"><img src="{{url('storage/app/public/Inicio/slider3.png')}}"></a>
+            <a class="carousel-item"><img src="{{url('storage/app/public/Inicio/slider4.png')}}"></a>
         </div>
 
 <br>
