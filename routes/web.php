ac298f8b115bae2b8de80d9ae67a6adec8bc846a
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-03-25 09:18:20 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -108,4 +108,4 @@ Route::post('/grafica','ControllerPedido@getgrafica');
 
 //---------------------Android
 Route::get('/android/catalogo','ControllerProducto@androidCatalogo');
-Route::post('/android/registro.usuario','ControllerUsuario@register');
+Route::get('/android/registro.usuario','ControllerUsuario@register');
