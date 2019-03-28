feec327a592c758926a116c00aa3a34411c8a93e
unknown <jorge.argumaniz@gmail.com>
2019-03-28 17:02:25 -0600

    metodo

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::post('/grafica','ControllerPedido@getgrafica');
 //---------------------Android
 Route::get('/android/catalogo','ControllerProducto@androidCatalogo');
 Route::post('/android/registro.usuario','ControllerUsuario@register');
-Route::get('/android/detalles', 'Controller@androidDetalles');
+Route::post('/android/detalles', 'Controller@androidDetalles');
 Route::post('/registro.usuario.android','ControllerUsuario@registerandroid');
 Route::get('/registro.usuario.androidv','ControllerUsuario@registerandroidv');
 
