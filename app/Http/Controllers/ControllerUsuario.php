cd2b5b3c217b055176458da6f6c14e3474b9f334
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-03-29 12:13:31 -0600

    Update ControllerUsuario.php

--- app/Http/Controllers/ControllerUsuario.php
+++ app/Http/Controllers/ControllerUsuario.php
@@ -117,7 +117,7 @@ class ControllerUsuario extends Controller
          $Usuario->e_mail=$r->input("email");
          $Usuario->tipo_usuario="0";
          $Usuario->pass=$r->input("contrasena");
-         //$usuario->token="SDKJFS";
+         $usuario->token= Str::random(60);
          $resul= $Usuario->save();
 
          $cliente= new Cliente;
