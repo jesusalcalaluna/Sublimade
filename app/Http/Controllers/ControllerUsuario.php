4b4304d9c293337e831e4e2b51743d580affdc6d
jesusalcalaluna <jesusalcalaluna@users.noreply.github.com>
2019-03-29 12:04:39 -0600

    Update ControllerUsuario.php

--- app/Http/Controllers/ControllerUsuario.php
+++ app/Http/Controllers/ControllerUsuario.php
@@ -113,11 +113,11 @@ class ControllerUsuario extends Controller
          $Usuario= new Usuario;
         //  $Usuario->id_persona= $request->input("id_usuario");
          $Usuario->id_persona= $id->id_persona;
-       
+
          $Usuario->e_mail=$r->input("email");
          $Usuario->tipo_usuario="0";
          $Usuario->pass=$r->input("contrasena");
-         $usuario->token="SDKJFS";
+         //$usuario->token="SDKJFS";
          $resul= $Usuario->save();
 
          $cliente= new Cliente;
@@ -216,13 +216,13 @@ class ControllerUsuario extends Controller
         $persona->f_nacimiento=$r->get('nacimiento');
         $persona->sexo=$r->get('sexo');
         $persona->save();
-      
+
 
           $id = DB::table('personas')->where('personas.tel_celular','=',$r->get('celular'))
          ->select('personas.id_persona')
          ->first();
 
-        
+
 
          //Session::put('id',$id->id_persona);
 
@@ -244,7 +244,6 @@ class ControllerUsuario extends Controller
 
      return $persona;
    }
- 
+
 
 }
-
