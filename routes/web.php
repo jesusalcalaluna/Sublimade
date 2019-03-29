b39624d3eecf7e66916e25373bbd2bfb2d40f08a
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-03-28 18:04:55 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -110,7 +110,7 @@ Route::post('/grafica','ControllerPedido@getgrafica');
 //---------------------Android
 Route::get('/android/catalogo','ControllerProducto@androidCatalogo');
 Route::post('/android/registro.usuario','ControllerUsuario@register');
-Route::post('/android/detalles', 'ControllerProducto@androidDetalles');
+Route::get('/android/detalles', 'Controller@androidDetalles');
 Route::post('/registro.usuario.android','ControllerUsuario@registerandroid');
-Route::get('/registro.usuario.androidv','ControllerUsuario@registerandroidv');
+Route::post('/registro.usuario.androidv','ControllerUsuario@validaciondeusuario');
 
