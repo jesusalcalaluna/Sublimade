df42287df298d7d8df01368281fed618f2327555
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-01 17:43:30 -0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,5 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::post('/registro.usuario.android','ControllerUsuario@registerandroid');
+Route::post('/registro.usuario.android','ControllerUsuario@registerandroid');
 
