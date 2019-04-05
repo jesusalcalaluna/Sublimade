494240f0ed500016e470e63553abe9ff49f1fa63
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-04 18:02:48 -0600

    a

--- routes/web.php
+++ routes/web.php
@@ -110,6 +110,5 @@ Route::post('/grafica','ControllerPedido@getgrafica');
 //---------------------Androi
 Route::get('/android/catalogo','ControllerProducto@androidCatalogo');
 Route::post('/android/detalles', 'ControllerProducto@androidDetalles');
-Route::post('/registro.android','ControllerUsuario@android');
 Route::get('/registro.usuario.androidv','ControllerUsuario@validaciondeusuario');
 
