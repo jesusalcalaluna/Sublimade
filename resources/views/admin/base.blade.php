8f88913efb53521ea5cb4577a060698d507f610e
JeffersonGutierritos <45271973+JeffersonGutierritos@users.noreply.github.com>
2019-04-04 22:05:10 -0600

    agregué icono

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -85,7 +85,7 @@
 
   <nav class="blue-grey ">
     <div class="nav-wrapper">
-      <a href="{{url('/')}}" style="font-size: 30px; font-family:'Dancing Script'">Sublimade</a>
+      <a href="{{url('/')}}" style="font-size: 50px; font-family:'Dancing Script'">Sublimade</a>
       <a class="black btn" style="margin-left: 10px; margin-top: -10px" href="{{'/'}}">Cliente</a>
       <a href="#" data-target="mobile-demo" class="sidenav-trigger show-on-large"><i class="material-icons">menu</i></a>
 
