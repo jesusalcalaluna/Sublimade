f4844bd43ae1e73961df900ac7556936ba3041e9
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-05 05:58:50 -0600

    Update ControllerUsuario.php

--- app/Http/Controllers/ControllerUsuario.php
+++ app/Http/Controllers/ControllerUsuario.php
@@ -251,8 +251,8 @@ class ControllerUsuario extends Controller
      $email = $r->get("usuario");
      $pass = $r->get("contrasena");
           $users = DB::table('usuarios')
-          ->where('usuarios.e_mail','=',"arturo99favela@gmail.com")
-          ->where('usuarios.pass', '=', "030117")  
+          ->where('usuarios.e_mail','=',$email)
+          ->where('usuarios.pass', '=', $pas) 
           ->get()->toJson();
 
         
