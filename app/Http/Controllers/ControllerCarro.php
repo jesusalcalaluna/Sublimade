803af303d9029e0bd01fb308154f0f1ea29d2b28
JeffersonGutierritos <45271973+JeffersonGutierritos@users.noreply.github.com>
2019-04-04 19:31:06 -0600

    prueba3 carrito

--- app/Http/Controllers/ControllerCarro.php
+++ app/Http/Controllers/ControllerCarro.php
@@ -39,8 +39,9 @@ class ControllerCarro extends Controller
         $costo_unitario = DB::table('productos')->select('productos.costo_unitario')->where('productos.id_producto','=',$id)->get();
         $total = $cantidad*$costo_unitario[0]->costo_unitario;
 
+        
 
-        $carrito_pivot = Carrito::find(Session::get('id'))->first();
+        $carrito_pivot = Carrito::find(Session::get('id'));
         $carrito_pivot->sub_total = 0;
         $carrito_pivot->productos()->save($carrito_pivot, ['id_carrito' =>Session::get('id'),'id_producto'=>$id, 'cantidad'=>$cantidad, 'total'=>$total,'talla'=>$talla]);
 
