ba9070657844f561d06b00761d2294981574315f
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-12 13:24:56 -0500

    Update ControllerProducto.php

--- app/Http/Controllers/ControllerProducto.php
+++ app/Http/Controllers/ControllerProducto.php
@@ -167,7 +167,7 @@ class ControllerProducto extends Controller
 $id=$r->get("id_deseado");
          $productos = DB::table('productos')
             ->join('deseados','deseados.productos_id_producto','=','productos.id_producto','inner')
-            ->join('usuarios','usuarios.id_persona','=','deseados.usuarios_id_persona','inner')->where("deseados.id_deseado", '=', $id)
+            ->join('usuarios','usuarios.id_persona','=','deseados.usuarios_id_persona','inner')->join('categorias','categorias.categoria','=','disenos.categoria','inner')->where("deseados.id_deseado", '=', $id)
             ->select(DB::raw("productos.nombre, productos.costo_unitario, disenos.diseno, categorias.categoria, productos.id_producto"))
             ->get();
 
