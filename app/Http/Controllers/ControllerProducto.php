3040456929bc5fb64c658c36c163f19dc799f95c
JeffersonGutierritos <45271973+JeffersonGutierritos@users.noreply.github.com>
2019-04-12 08:03:50 -0500

    intento 8

--- app/Http/Controllers/ControllerProducto.php
+++ app/Http/Controllers/ControllerProducto.php
@@ -146,7 +146,7 @@ class ControllerProducto extends Controller
 
     function androidcategorias(){
         $categorias=DB::table('categorias')->get();
-        return $categorias->categoria;
+        return $categorias[0];
     }
 
 }
