34d1711339d20b788d80ad4cd2da39640317f07b
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-12 15:30:59 -0500

    Update ControllerProducto.php

--- app/Http/Controllers/ControllerProducto.php
+++ app/Http/Controllers/ControllerProducto.php
@@ -169,7 +169,7 @@ $id=$r->get(1);
             ->join('deseados','deseados.productos_id_producto','=','productos.id_producto','inner')
             ->join('usuarios','usuarios.id_persona','=','deseados.usuarios_id_persona','inner')
             ->join('disenos','disenos.id_diseno','=','productos.id_diseno','inner')
-            ->join('categorias','categorias.categoria','=','disenos.categoria','inner')->where("deseados.id_deseados", '=', $id)
+            ->join('categorias','categorias.categoria','=','disenos.categoria','inner')->where("deseados.usuarios_id_persona", '=', $id)
             ->select(DB::raw("productos.nombre, productos.costo_unitario, disenos.diseno, categorias.categoria, productos.id_producto"))
             ->get();
 
