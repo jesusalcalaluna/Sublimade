e55f3546322981aa383976623f9ead05072ce8e0
unknown <jorge.argumaniz@gmail.com>
2019-04-12 12:53:52 -0500

    carrito

--- app/Http/Controllers/ControllerCarro.php
+++ app/Http/Controllers/ControllerCarro.php
@@ -101,5 +101,20 @@ class ControllerCarro extends Controller
         $carrito_pivot->productos()->save($carrito_pivot, ['id_carrito' =>$id_usuario,'id_producto'=>$id, 'cantidad'=>$cantidad, 'total'=>$total,'talla'=>$talla]);
 
     }
+    function vercarritoandroid(Request $r){
+        $id = $r->get(1);
+
+        $producto = DB::table('productos')->join('disenos','disenos.id_diseno','=','productos.id_diseno','inner')
+            ->join('categorias','categorias.categoria','=','disenos.categoria','inner')
+            ->join('tipos_producto', 'tipos_producto.id_tipo_producto', '=','productos.id_tipo_producto', 'inner')
+            ->join('carritos_has_productos', 'carritos_has_productos.id_producto','=','productos.id_producto', 'inner')
+            ->join('carritos','carritos.id_carrito','=','carritos_has_productos.id_carrito', 'inner')
+            ->select(DB::raw("carritos.sub_total as 'sub_total', carritos_has_productos.cantidad as 'cantidad', carritos_has_productos.total as 'total', carritos_has_productos.talla as 'talla', carritos.id_carrito as 'id_carrito', productos.sexo as 'sexo', productos.nombre as 'nombre', disenos.diseno as 'diseno', productos.costo_unitario as 'costo', tipos_producto.nombre as 'tipo', disenos.categoria as 'categoria', productos.id_producto as 'id_producto'"))
+            ->where('carritos_has_productos.id_carrito','=',$id)
+            ->get();
+
+        return $producto;
+
+    }
 
 }
