8ecab09a532f72e5a60e55ca0ea43b4c538ea5f0
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-13 17:11:07 -0500

    a

--- app/Deseado.php
+++ app/Deseado.php
@@ -9,4 +9,18 @@ class Deseado extends Model
      protected $table='deseados';
         protected $primarykey='id_deseados';
          public $timestamps=false;
+
+
+          public function usuarios()
+    {
+    	
+    	 	return $this->belongsTo(Persona::class ,'id_deseados','id_persona');
+	    
+    }
+      public function producto()
+    {
+      
+        return $this->belongsTo(Producto::class ,'id_deseados','id_producto');
+      
+    }
 }
