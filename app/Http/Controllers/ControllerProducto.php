b51a6ab59c2f890fab5f76c7f85ffa5cd4c32718
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2019-04-13 19:09:54 -0500

    Update ControllerProducto.php

--- app/Http/Controllers/ControllerProducto.php
+++ app/Http/Controllers/ControllerProducto.php
@@ -182,11 +182,11 @@ class ControllerProducto extends Controller
           $id = DB::table('deseados')->where('deseados.productos_id_producto','=',$id_producto)
          ->select('deseados.id_deseados')
          ->get();
-     
+     $d= Deseado::where("id_deseados","=",$id_producto);
 
 //      $d= Deseado::find($id);
    //   $d->delete();
-    return $id;
+    return $d;
 
     }
 
