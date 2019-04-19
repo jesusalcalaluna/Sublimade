6adda690a9fb4d2da43605fa4c169067395cecae
unknown <jorge.argumaniz@gmail.com>
2019-04-18 21:59:05 -0500

    gateway

--- app/Http/Controllers/ControllerBraintree.php
+++ app/Http/Controllers/ControllerBraintree.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 
+use Braintree\Gateway;
 use Illuminate\Http\Request;
 use Test\Unit\GatewayTest;
 
 class ControllerBraintree extends Controller
 {
     function client_token(){
-        $gateway = new \Braintree_Gateway([
+
+        $gateway = new Gateway([
             'environment' => 'sandbox',
             'merchantId' => 'mxxfwff3fs9st5dr',
             'publicKey' => 'hmfpqy87s8nz6brx',
@@ -24,7 +26,7 @@ class ControllerBraintree extends Controller
     function payment_method_nonce(Request $r){
         $nonceFromTheClient = $r->get('payment_method_nonce');
         $monto = $r->get('monto');
-        $gateway = new \Braintree_Gateway([
+        $gateway = new Gateway([
             'environment' => 'sandbox',
             'merchantId' => 'mxxfwff3fs9st5dr',
             'publicKey' => 'hmfpqy87s8nz6brx',
