217c7b1c4137c72867c5914d885f03c4f24f241f
JeffersonGutierritos <45271973+JeffersonGutierritos@users.noreply.github.com>
2019-04-25 20:32:34 -0500

    Ya funcionan filtros, hay que checar consultas que no traen precio

--- app/Http/Controllers/ControllerProducto.php
+++ app/Http/Controllers/ControllerProducto.php
@@ -207,8 +207,10 @@ class ControllerProducto extends Controller
         $producto=$r;
         if($producto->get(1)=='all' && $producto->get(2)=='all' && $producto->get(0)=='all'){
             $filtro = DB::table('productos')->join('disenos','disenos.id_diseno','=','productos.id_diseno','inner')
-                ->join('categorias','categorias.categoria','=','disenos.categoria','inner')->join('tipos_producto', 'tipos_producto.id_tipo_producto', '=','productos.id_tipo_producto')
-                ->select(DB::raw("productos.sexo as 'sexo', productos.nombre as 'nombre', disenos.diseno as 'diseno', productos.costo_unitario as 'costo', tipos_producto.nombre as 'tipo', disenos.categoria as 'categoria', productos.id_producto as 'id_producto'"))->get();
+                ->join('categorias','categorias.categoria','=','disenos.categoria','inner')
+                ->join('tipos_producto', 'tipos_producto.id_tipo_producto', '=','productos.id_tipo_producto')
+                ->select(DB::raw("productos.sexo as 'sexo', productos.nombre as 'nombre', disenos.diseno as 'diseno', productos.costo_unitario as 'costo', tipos_producto.nombre as 'tipo', disenos.categoria as 'categoria', productos.id_producto as 'id_producto'"))
+                ->get();
             return $filtro;
 
         }elseif ($producto->get(1)=='all' && $producto->get(2)=='all' && $producto->get(0)!='all') {
