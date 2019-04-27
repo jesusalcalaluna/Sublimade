b7a6a32dc2f8a8ae37cbdbdeddfa8f241364543e
unknown <jorge.argumaniz@gmail.com>
2019-04-27 17:24:39 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -118,4 +118,4 @@ Route::post('/android/carrito', 'ControllerCarro@vercarritoandroid');
 Route::post('/android/eliminarcarrito', 'ControllerCarro@deleteprodandroid');
 Route::post('/consulta', 'ControllerCarro@consulta');
 Route::get('/pagoAndroid','ControllerCarro@pagoAndroid');
-Route::post('/generarPedidoAndroid/{id}',['as'=>'generarPedidoAndroid', 'uses'=>'ControllerPedido@generarPedidoAndroid']);
+Route::get('/generarPedidoAndroid/{id}',['as'=>'generarPedidoAndroid', 'uses'=>'ControllerPedido@generarPedidoAndroid']);
